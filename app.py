ee395742a34bf9d0dc131bc53316821f000c01d8
Luca Cozzi <cozziluca1@gmail.com>
2026-03-18 19:19:29 +0100

    Svillupato sistema di calcollagio cash
    Sono andato a sviluppare un semplicissimo sistema di calcolo per capire in quanto tempo avrei raggiunto i miei obbiettivi finanziari

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ amico3 = "gaia"
 
 aggetivo = "e stupido"
 
-print(f"{amico1} {aggetivo} ")
-print(f"{amico2} {aggetivo}")
-print(f"{amico3} {aggetivo}")
+#print(f"{amico1} {aggetivo} ")
+#print(f"{amico2} {aggetivo}")
+#print(f"{amico3} {aggetivo}")
+
+
+Risparmio = 11
+mesi = 6
+target = 100
+
+os = target / Risparmio
+
+Accumullo = Risparmio * mesi 
+Attualita = target - Accumullo
+
+tempo =  Attualita / Risparmio
+
+
+print(f"Per addeso hai risparmiato {Accumullo}")
+print(f"Per il tuo target ti manca {Attualita}")
+print(f"Continuando cosi ci vorra ancora {tempo} per raggiungere l'obbietivo")
+print(os)
