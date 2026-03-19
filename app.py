cd6178952efc7c90a18fc56ed4e6040ffcee9a35
Luca Cozzi <cozziluca1@gmail.com>
2026-03-19 20:52:32 +0100

    check login e es1

--- app.py
+++ app.py
@@ -29,7 +29,46 @@ tempo =  Attualita / Risparmio
 
 Pasquetta = True
 
-if Pasquetta:
-    print("spacchiammo tutto")
-else:
-    print("organnizziamo")
+#if Pasquetta:
+    #print("spacchiammo tutto")
+#else:
+    #print("organnizziamo")
+
+
+
+#base per costruire un piccolo check campo per login o altro 
+#name = input("compila con il tuo nome: ")
+
+#name = bool(name)
+
+#Nome = name
+
+#if Nome:
+ #   print("puoi proccedere")
+#else: 
+#    print("compila il campo nome")
+
+
+#ES 1 rettangolo 
+
+name = input("inserisci il tuo nome: ")
+
+print(f"ciao {name} oggi andiamo a costruire un rettangolo e a calcolare la sua aerea")
+
+altezza = input("inserisci l'altezza del nostro rettangolo: ")
+
+lunghezza = input("inserisci la lunghezza del nostro rettangolo: ")
+
+lunghezza = True 
+while lunghezza > altezza :
+    print(f"la lunghezza non puo essere minore dell'altezza che è {altezza}, perfafore inserisci un altro valore")
+
+
+A = int(altezza)
+
+L = int(lunghezza)
+
+Area = A * L
+
+print(f"ecco l'area del nostro rettangolo : {Area}, bel lavoro {name} ")
+
